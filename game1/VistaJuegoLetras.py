2a28e216f3d7f502c99b3171c6f9bab9740f31b2
SGarcia710 <sgarcia710@outlook.com>
2019-04-16 23:30:43 -0500

    Cambios Vista Letras

--- game1/VistaJuegoLetras.py
+++ game1/VistaJuegoLetras.py
@@ -14,7 +14,8 @@ class VistaJuegoLetras:
   Y = 700
   def __init__(self,parentWindow):
     self.juego = JuegoLetras()
-    self.root = Toplevel()#cambiar por TopLevel()
+    self.fechaInicio = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    self.root = Toplevel()
     self.parentWindow = parentWindow
     self.root.title("Control Inihibitorio")
     self.root.config(heigh=self.Y, width=self.X)
@@ -30,60 +31,59 @@ class VistaJuegoLetras:
     
     self.root.geometry("+{}+{}".format(positionRight, positionDown))
 
-    # self.label1 = Label(self.root, text = "")
-    # self.label1.config(font=("Righteous", 30), bg = "white")
-    # self.label1.pack()
-    # self.label1.place(anchor=CENTER, x=self.X/2, y= self.Y/6)
+    self.label1 = Label(self.root, text = "")
+    self.label1.config(font=("Righteous", 30), bg = "white")
+    self.label1.pack()
+    self.label1.place(anchor=CENTER, x=self.X/2, y= self.Y/6)
 
-    # self.iniciarJuego()
+    self.labelIzq = Label(self.root, text = "letraStr")
+    self.labelIzq.config(font=("Righteous", 50), bg = "white", bd = 2, relief = GROOVE, padx = 15)
+    self.labelIzq.pack()
+    self.labelIzq.place(anchor=CENTER, x=200, y= self.Y/2)
+
+    self.labelDer = Label(self.root, text = '_')
+    self.labelDer.config(font=("Righteous", 50), bg = "white", bd = 2, relief = GROOVE, padx = 15, fg = "white")
+    self.labelDer.pack()
+    self.labelDer.place(anchor=CENTER, x=self.X-200, y = self.Y/2)
+
+    self.botonZ = Button(self.root, text = "Z", bd = 1, relief = GROOVE, bg = RED , fg = "white", font=("Arial", 16))
+    self.botonZ.pack()
+    self.botonZ.place(anchor=CENTER, x = 300, y = self.Y-150, width = 150, heigh = 50)
+
+    self.botonG = Button(self.root, text = "-", bd = 1, relief = GROOVE, bg = RED, fg = "white", font=("Arial", 16))
+    self.botonG.pack()
+    self.botonG.place(anchor=CENTER, x = self.X-300, y = self.Y-150, width = 150, heigh = 50)
+      
     self.pintarNivel()
 
     self.root.mainloop()
 
-  # def iniciarJuego(self):
-
-
   def pintarNivel(self):
     nivel = self.juego.obtenerNivel()
 
+    self.botonG.configure(command= lambda: self.presionarBoton(nivel,nivel.TECLA2))
+    self.botonZ.configure(command= lambda: self.presionarBoton(nivel,nivel.TECLA1))
+
     if nivel is None:
-      print("ano")
+      cadenaLog, cadenaMsg = self.crearResultado()
+      print(cadenaLog)
+      print(cadenaMsg)
+
     else:
       self.tiempoActual = datetime.now()
-      nivelActualStr = "Nivel "+str(nivel.numNivel)
-      label1 = Label(self.root, text = nivelActualStr)
-      label1.config(text = nivelActualStr)
-      label1.pack()
-      label1.place(anchor=CENTER, x=self.X/2, y= self.Y/6)
+      nivelActualStr = "Nivel " +str(nivel.numNivel)
+      self.label1.config(text = nivelActualStr)
       letraStr = nivel.letra.letra
-      if nivel.lado == nivel.IZQ:
-        labelIzq = Label(self.root, text = letraStr)
-        labelIzq.config(font=("Righteous", 50), bg = "white", bd = 2, relief = GROOVE, padx = 15)
-        labelIzq.pack()
-        labelIzq.place(anchor=CENTER, x=200, y= self.Y/2)
-        labelDer = Label(self.root, text = '_')
-        labelDer.config(font=("Righteous", 50), bg = "white", bd = 2, relief = GROOVE, padx = 15, fg = "white")
-        labelDer.pack()
-        labelDer.place(anchor=CENTER, x=self.X-200, y = self.Y/2)
-      else: 
-        labelIzq = Label(self.root, text = '_')
-        labelIzq.config(font=("Righteous", 50), bg = "white", bd = 2, relief = GROOVE, padx = 15, fg = "white")
-        labelIzq.pack()
-        labelIzq.place(anchor=CENTER, x=200, y= self.Y/2)
-        labelDer = Label(self.root, text = letraStr)
-        labelDer.config(font=("Righteous", 50), bg = "white", bd = 2, relief = GROOVE, padx = 15)
-        labelDer.pack()
-        labelDer.place(anchor=CENTER, x=self.X-200, y = self.Y/2)
-      
-      botonZ = Button(self.root, text = "Z", bd = 1, relief = GROOVE, bg = RED , fg = "white", font=("Arial", 16))
-      botonZ.configure(command= lambda: self.presionarBoton(nivel,nivel.TECLA1) )
-      botonZ.pack()
-      botonZ.place(anchor=CENTER, x = 300, y = self.Y-150, width = 150, heigh = 50)
 
-      botonG = Button(self.root, text = "-", bd = 1, relief = GROOVE, bg = RED, fg = "white", font=("Arial", 16))
-      botonG.configure(command= lambda: self.presionarBoton(nivel,nivel.TECLA2)  )
-      botonG.pack()
-      botonG.place(anchor=CENTER, x = self.X-300, y = self.Y-150, width = 150, heigh = 50)
+      if nivel.lado == nivel.IZQ:
+        print("IZQ "+letraStr)
+        self.labelIzq.config(text=letraStr, fg = "black")
+        self.labelDer.config(text='_', fg = "white")
+      else: 
+        self.labelIzq.config(text='_', fg = "white")
+        print("DER "+letraStr)
+        self.labelDer.config(text=letraStr, fg = "black")
+
       if nivel.trampa:
         hilo1 = threading.Thread(target=self.ejecutarPausa)
         hilo1.start()
@@ -92,16 +92,42 @@ class VistaJuegoLetras:
     time.sleep(5)
     self.mostrarMensaje()
 
-
   def presionarBoton(self,nivel,tecla):
     nivel.presionarBoton(tecla)
     ahora = datetime.now()
     transcurrido = ahora - self.tiempoActual
     nivel.tiempoReaccion = transcurrido
+
     if not nivel.trampa:
       self.mostrarMensaje()
-      
 
   def mostrarMensaje(self):
     mbox.showinfo("Felicitaciones!", "Nivel terminado.")
     self.pintarNivel()
+
+  def crearResultado(self):
+    cadenaLog = "[Nivel 2]" + self.fechaInicio + ","
+    niveles = self.juego.nivelesJuego
+    for nivel in niveles:
+      tiempoR = nivel.tiempoReaccion
+      print(type(tiempoR))
+      # strftime('%Y-%m-%d %H:%M:%S')
+      segundos = tiempoR.seconds
+      # minutos = tiempoR.minutes
+      minutos = "0"
+      resultado = None
+      if nivel.trampa and nivel.botonPresionado  == None:
+        resultado = "Acierto"
+      elif not nivel.trampa and nivel.lado == nivel.IZQ and nivel.botonPresionado == nivel.IZQ:
+        resultado = "Acierto"
+      elif not nivel.trampa and nivel.lado == nivel.DER and nivel.botonPresionado == nivel.DER:
+        resultado = "Acierto"
+      else:
+        resultado = "Error"
+
+      if nivel.trampa:
+        cadenaLog += " T.R." + nivel.numNivel + " 0:0 " + resultado + ","
+      else:
+        cadenaLog += " T.R." + nivel.numNivel + " " + minutos + ":" + segundos + " " + resultado + ","
+    
+    return cadenaLog, "Pendiente"
