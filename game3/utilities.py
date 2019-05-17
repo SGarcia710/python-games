4fbe823ab473496d88731209b647b21745fe210b
sgarcia710 <sgarcia710@outlook.com>
2019-05-17 02:39:49 -0500

    cambio nombre txt juego 3

--- game3/utilities.py
+++ game3/utilities.py
@@ -7,7 +7,7 @@ BLUE = "#01CBCF"
 RED = "#f96262"
 GREEN = "#0ce59a"
 DARK = "#404e67"
-archivoTXT = "game3/VelocidadDeProcesamiento.txt"
+archivoTXT = "game3/MemoriaTrabajoFonologica.txt"
 
 def numeroAleatorio(x, y):
   return random.randint(x,y)
